319d4e211dc4103a96d2558893dc6cb312e489e4
John <johneliud4@gmail.com>
2026-09-23 19:05:19 +0300

    feat: enable global notifications, home page sections, and build optimizations.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Footer } from './shared/components/footer/footer';
 import { ModalHostComponent } from './shared/components/modal/modal-host';
+import { NotificationComponent } from './shared/components/notification/notification';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, ModalHostComponent],
+  imports: [RouterOutlet, Header, Footer, ModalHostComponent, NotificationComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
